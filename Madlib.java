7dc6b5f932854a35acc780f57e4f59ea01538295
Julian Damik <julian.damik@techhigh.us>
2015-09-14 08:05:08 -0400

    Not finished

---
+++ Madlib.java
@@ -0,0 +1,32 @@
+import java.util.Scanner
+
+public class MadLib{
+   public static void main(String args[]){
+   
+   Scanner scnr = new Scanner(System.in);
+   
+   String color = "";
+   String superlative = "";
+   String adjectiveOne = "";
+   String bodyPartOne = "";
+   String bodyPartTwo = "";
+   String noun = "";
+   String Animal = "";
+   String adjectiveTwo = "";
+   String adjectiveThree = "";
+   Stinring adjectiveFour = "";
+   
+   System.out.ptintln("Enter a color : ");
+   color = scnr.nextLine();
+   System.out.ptintln("Enter a superlative. (Ending in 'est'): ");
+   superlative = scnr.nextLine();
+   System.out.ptintln("Enter an adjective: "
+   adjectiveOne = scnr.nextLine();
+   System.out.ptintln("Enter a plural body part: ");
+   bodyPartOne = scnr.nextLine():
+   System.out.ptintln("Enter a noun: ");
+   noun = scnr.nextLine();
+   System.out.ptintln("Enter a plural animal: ");
+   animal = scnr.nextLine();
+   System.out.ptintln("Enter an adjective: ");
+   adjectiveTwo = scnr.nextLine();

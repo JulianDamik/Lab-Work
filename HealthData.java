1190e24abcff709b2be960876b2fb2f80e4e7738
Julian Damik <julian.damik@techhigh.us>
2015-09-16 07:49:32 -0400

    Finished

--- HealthData.java
+++ HealthData.java
@@ -11,6 +11,7 @@ public class HealthData {
       int beatsPerMin = 0;
       double numOfSneezes = 0;
       int calsBurned = 0;
+      int breathsPerDay = 0;
       
       System.out.println("Enter your age in years: ");
       userAgeYears = scnr.nextInt();
@@ -22,7 +23,7 @@ public class HealthData {
       beatsPerMin = minutes * 72;
       numOfSneezes = userAgeDays * 1.2;
       calsBurned = userAgeDays * 1620;
-      
+      breathsPerDay = userAgeDays * 28800; 
 
       System.out.println("You are " + userAgeDays + " days old.");
       System.out.println("You are about " + hours + " hours old.");
@@ -31,7 +32,7 @@ public class HealthData {
       System.out.println("Your heart has beated about " + beatsPerMin + " times.");
       System.out.println("You have sneezed about " + numOfSneezes + " times in your life.");
       System.out.println("You have burned about " + calsBurned + " in your life.");
-
+      System.out.println("You have taken about " + breathsPerDay + " breaths in your life time.");      
 
 
       

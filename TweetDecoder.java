2cc861cec975d24f8b0b7ddc4feb9816c2052574
Julian Damik <julian.damik@techhigh.us>
2015-09-22 09:24:23 -0400

    Finished one and two

--- TweetDecoder.java
+++ TweetDecoder.java
@@ -4,53 +4,54 @@ public class TweetDecoder {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
       String origTweet = "";
+      String userTweet = "";
 
       System.out.println("Enter abbreviation from tweet: ");
       origTweet = scnr.next();
 
-      if (origTweet.equals("LOL")) {
+      if (origTweet.equalsIgnoreCase("LOL")) {
          System.out.println("LOL = laughing out loud");
          }
-      else if ((origTweet.equals("LLO"))||(origTweet.equals("OLL"))){
+      else if ((origTweet.equalsIgnoreCase("LLO"))||(origTweet.equalsIgnoreCase("OLL"))){
             System.out.println("Did you mean LOL? LOL = laughing out loud");
       }
-      else if (origTweet.equals("BFN")) {
+      else if (origTweet.equalsIgnoreCase("BFN")) {
          System.out.println("BFN = bye for now");
       }
-      else if ((origTweet.equals("BNF"))||(origTweet.equals("NFB"))){
+      else if ((origTweet.equalsIgnoreCase("BNF"))||(origTweet.equalsIgnoreCase("NFB"))){
             System.out.println("Did you mean BFN? BFN = bye for now");
       }
-      else if (origTweet.equals("FTW")) {
+      else if (origTweet.equalsIgnoreCase("FTW")) {
          System.out.println("FTW = for the win");
       }
-       else if ((origTweet.equals("FWT"))||(origTweet.equals("TWF"))){
+       else if ((origTweet.equalsIgnoreCase("FWT"))||(origTweet.equalsIgnoreCase("TWF"))){
             System.out.println("Did you mean FTW? FTW = for the win");
       }
-      else if (origTweet.equals("IRL")) {
+      else if (origTweet.equalsIgnoreCase("IRL")) {
          System.out.println("IRL = in real life");
       }
-       else if ((origTweet.equals("RIL"))||(origTweet.equals("LRI"))){
+       else if ((origTweet.equalsIgnoreCase("RIL"))||(origTweet.equalsIgnoreCase("LRI"))){
             System.out.println("Did you mean IRL? IRL = in real life");
       }
-      else if (origTweet.equals("DM")) {
+      else if (origTweet.equalsIgnoreCase("DM")) {
          System.out.println("DM = direct message");
       }
-       else if (origTweet.equals("MD")){
+       else if (origTweet.equalsIgnoreCase("MD")){
             System.out.println("Did you mean DM? DM = Direct message");
       }
-      else if (origTweet.equals("FF")) {
+      else if (origTweet.equalsIgnoreCase("FF")) {
          System.out.println("FF = follow friday");
       }
-      else if (origTweet.equals("RT")) {
+      else if (origTweet.equalsIgnoreCase("RT")) {
          System.out.println("RT = retweet");
       }
-       else if (origTweet.equals("LLO")){
+       else if (origTweet.equalsIgnoreCase("TR")){
             System.out.println("Did you mean RT? RT = Retweet");
       }
         else {
          System.out.println("Sorry, don't know that one.");
       }
-
-      return;
+        
+           return;
    }
 }
